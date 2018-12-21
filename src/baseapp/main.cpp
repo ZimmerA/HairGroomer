8c3579473f9170adff106fdb2079f3e180f3cd38
Adrian Zimmer <z.adrian1995@gmail.com>
2018-12-21 19:17:04 +0100

    Added commandline option to use bright theme

--- src/baseapp/main.cpp
+++ src/baseapp/main.cpp
@@ -17,28 +17,32 @@ int main(int argc, char* argv[])
 	QCoreApplication::setOrganizationName("Adrian Zimmer");
 	QCoreApplication::setApplicationVersion("1.0");
 
-	// Load dark theme
-	QFile f(":qdarkstyle/style.qss");
-	if (!f.exists())
-	{
-		printf("Unable to set stylesheet, file not found\n");
-	}
-	else
-	{
-		f.open(QFile::ReadOnly | QFile::Text);
-		QTextStream ts(&f);
-		qApp->setStyleSheet(ts.readAll());
-	}
 
 	// Setup the command line argument Parser
 	QCommandLineParser parser;
 	parser.addHelpOption();
 	parser.addVersionOption();
 	const QCommandLineOption fbx_model_option("fbx", QCoreApplication::translate("main", "Load the provided model on application startup"), QCoreApplication::translate("main", "path"));
+    const QCommandLineOption bright_theme_option(QStringList() << "b" << "bright",QCoreApplication::translate("main", "Use the bright UI theme."));
+    parser.addOption(bright_theme_option);
 	parser.addOption(fbx_model_option);
 
 	// Process the actual command line arguments given by the user
 	parser.process(a);
+
+	// Load dark theme
+	QFile f(":qdarkstyle/style.qss");
+	if (!f.exists())
+	{
+		printf("Unable to set stylesheet, file not found\n");
+	}
+	else if(!parser.isSet(bright_theme_option))
+	{
+		f.open(QFile::ReadOnly | QFile::Text);
+		QTextStream ts(&f);
+		qApp->setStyleSheet(ts.readAll());
+	}
+
 
 	// OpenGL version
 	QSurfaceFormat fmt;
